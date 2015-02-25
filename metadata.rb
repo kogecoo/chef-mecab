099615069dd0ef465c7465eaed1cbaf117d56515
kogecoo <kogecoo@gmail.com>
2015-02-25 20:56:50 +0900

    bump version to 0.2.0.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'kogecoo@gmail.com'
 license          'Apache 2.0'
 description      'Installs MeCab and its well-known dictionary.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 supports 'ubuntu'
 supports 'debian'
